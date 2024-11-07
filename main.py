5b95fb7b02787d782b8d27bb4434598543776d03
seojm <myungs8562@gmail.com>
2024-11-07 15:11:46 +0900

    Feat: get resultInfo from client

--- main.py
+++ main.py
@@ -1,24 +1,68 @@
-from typing import Union
+from typing import Union, List
 from pydantic import BaseModel
 from fastapi import FastAPI
+from time import time
+from fastapi.middleware.cors import CORSMiddleware
+
+
+import httpx
+import asyncio
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://www.mopl.kr"],  # Next.js 앱 주소
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+API_URL = "https://dev.mopl.kr/api/v1"
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+class IncorrectProblem(BaseModel):
+    problemNumber: str
+    correctRate: float
+
+class TestResult(BaseModel):
+    id: int
+    score: int
+    solvingTime: str
+    averageSolvingTime: str
+    estimatedRating: int
+    incorrectProblems: List[IncorrectProblem]
+
+async def requestPracticeTests(client):
+    response = await client.get(f"{API_URL}/practiceTests/2")
+    return response.text
+
+# async def requestRating(client, practiceTestId):
+#     response = await client.get(f"{API_URL}/rating/{practiceTestId}")
+#     return response.text
+
+async def task():
+    async with httpx.AsyncClient() as client:
+        tasks = [requestPracticeTests(client) for i in range(100)]
+        result = await asyncio.gather(*tasks)
+        print(result)
+
+
+@app.get("/", tags=["root"])
+async def read_root():
+    # start = time()
+    # await task()
+    # print("time: ", time() - start)
+    return 'Hello'
+    
+
+@app.post("/test/resultInfo")
+async def get_result_info_from_client(test_result: TestResult):
+    return {"message": "Data received successfully"}
+
+
+@app.get("/test/rating/{practiceTestId}", tags=["test"])
+async def get_rating(practiceTestId: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{API_URL}/rating/{practiceTestId}")
+    return response.text
