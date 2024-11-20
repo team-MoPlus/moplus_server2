51a80ca039a578692bb2966ad2c5520cf5782d6b
seojm <myungs8562@gmail.com>
2024-11-21 00:12:53 +0900

    Feat: pdf generation and downloadable

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from typing import Union, List
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from time import time
 from fastapi.middleware.cors import CORSMiddleware
-
-
+from dotenv import load_dotenv
+<<<<<<< HEAD
+from models import DetailResultApplication, TestResult
+=======
+from models import TestResult
+>>>>>>> main
+from pdfmain import create_review_note
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException
+import crud, models
 import httpx
 import asyncio
+import os
 
-from pdfmain import create_review_note
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -21,20 +35,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-API_URL = "https://dev.mopl.kr/api/v1"
 
-
-class IncorrectProblem(BaseModel):
-    problemNumber: str
-    correctRate: float
-
-class TestResult(BaseModel):
-    id: int
-    score: int
-    solvingTime: str
-    averageSolvingTime: str
-    estimatedRating: int
-    incorrectProblems: List[IncorrectProblem]
+# Dependency Injection 
+def get_db():
+    db = SessionLocal()
+    try : 
+        yield db
+    finally:
+        db.close()
 
 async def requestPracticeTests(client):
     response = await client.get(f"{API_URL}/practiceTests/2")
@@ -61,7 +69,17 @@ async def read_root():
 
 @app.post("/test/resultInfo")
 async def get_result_info_from_client(test_result: TestResult):
-    return {"message": "Data received successfully"}
+    return {"message": "Data received successfully", "response": test_result}
+
+@app.post("/detailResultApplication")
+async def get_detail_result_application_from_client(detail_result: DetailResultApplication):
+    try:
+        detail_result = detail_result.model_dump()
+        await create_review_note(detail_result)
+
+        return {"status": "OK"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/test/rating/{practiceTestId}", tags=["test"])
@@ -71,18 +89,31 @@ async def get_rating(practiceTestId: str):
     return response.text
 
 
-# 사진 다운로드
-# @app.get("/image/{imageId}")
-# async def get_image_by_id(photo_id: int, db: Session = Depends(get_db)):
-#     find_photo: Photo = db.query(Photo).filter_by(photo_id=photo_id).first()
-#     return FileResponse(find_photo.src)
+<<<<<<< HEAD
+=======
 
+>>>>>>> main
+@app.get("/image-url")
+def get_image_url(problem_id: int, db:Session=Depends(get_db)):
+    problem_image_info = crud.get_image(db, problem_id)
+    return problem_image_info.image_url
+
+# 복습표 생성 요청
+@app.post("/request-review")
+async def request_review_note():
+    try:
+        # problem_info = crud.get_image(db, skip=0, limit=0)
+        await create_review_note()
+
+        return {"status": "OK"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
 # PDF 다운로드 엔드포인트
 @app.get("/download-review")
 async def download_review_note():
-    await create_review_note()
-
+    
     # PDF 파일 다운로드
     response = FileResponse(path="./output.pdf", filename='download_review.pdf', media_type="application/pdf")
     
--- pdfmain.py
+++ pdfmain.py
@@ -1,12 +1,15 @@
-import os
+import os, re
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.colors import HexColor
-from utils import draw_vertical_orange
+from models import DetailResultApplication
+from utils import draw_dashed_box, draw_heading, draw_page_number, draw_problem_page, draw_vertical_orange
 
-async def create_review_note():
+pattern = r"PT(?:(\d+)H)?(?:(\d+)M)?"
+
+def create_review_note(data: DetailResultApplication):
     # 현재 파일의 디렉토리 경로를 기준으로 폰트 경로 설정
     # base_dir = os.path.dirname(__file__)
     
@@ -24,17 +27,8 @@ async def create_review_note():
     grey_color = HexColor('#828282')
     black_color = HexColor("#333333")
 
-    # 모플 로고
-    logo = "모플"
-    c.setFont("Pretendard-Regular", 40)
-    c.setFillColor(orange_color)
-    str_width = c.stringWidth(logo)
-    c.drawString(20, 785, logo)
-    
-    # 가로선
-    c.setStrokeColor(lightgrey_color)
-    c.setLineWidth(4)
-    c.line(20, 770, 575, 770)
+    draw_heading(c, orange_color, lightgrey_color)
+    draw_page_number(c, "1")
 
     ##### 결과 #####
     # 세로선 그리기 (오렌지 색상)
@@ -48,7 +42,7 @@ async def create_review_note():
     # 점수
     c.setFillColor(orange_color)
     c.setFont("Pretendard-Regular", 80)
-    c.drawString(35, height - 230, "80")
+    c.drawString(35, height - 230, f'{data["score"]}')
 
     c.setFillColor(black_color)
     c.setFont("Pretendard-Regular", 80)
@@ -61,7 +55,7 @@ async def create_review_note():
     # 내 등급
     c.setFillColor(orange_color)
     c.setFont("Pretendard-Regular", 40)
-    c.drawString(245, height - 230, "3")
+    c.drawString(245, height - 230, f'{data["estimatedRatingGetResponses"][0]["estimatedRating"]}')
 
     c.setFillColor(black_color)
     c.setFont("Pretendard-Regular", 40)
@@ -72,10 +66,11 @@ async def create_review_note():
     c.drawString(280, height - 256, "내 등급")
 
     # 내 풀이 시간
+    match = re.match(pattern, data["solvingTime"])
     c.setFillColor(orange_color)
     c.setFont("Pretendard-Regular", 40)
-    c.drawString(392, height - 230, "1")
-    c.drawString(440, height - 230, "10")
+    c.drawString(392, height - 230, f'{match.group(1) if match else 0}')
+    c.drawString(440, height - 230, f'{match.group(2) if match else 0}')
 
     c.setFillColor(black_color)
     c.setFont("Pretendard-Regular", 40)
@@ -94,11 +89,37 @@ async def create_review_note():
     c.setFont("Pretendard-Bold", 24)
     c.drawString(30, height - 307, "전체 틀린 문제")  # 텍스트 위치 설정
 
+    ## 문제들
+    text_y = 500
+    for i, problem in enumerate(data["incorrectProblems"]):
+        
+        if i%5 == 0 and i > 0:
+            text_y -= 30
+        
+        c.setFillColor(black_color)
+        c.setFont("Pretendard-Regular", 24)
+        p_num = f'{problem["problemNumber"]}번'
+        c.drawString(25 + (i%5)*110, text_y, p_num)
+
+        p_num_width = c.stringWidth(p_num, "Pretendard-Regular", 24)
+
+        box_width = 40
+        box_height = 20
+        box_x = 30 + (i%5)*110 + p_num_width
+        box_y = text_y
+
+        # 박스 그리기 (배경)
+        c.setLineWidth(1)  # 테두리 두께 설정 (얇게)
+        c.setStrokeColor(HexColor("#FFA500"))  # 주황색 테두리
+        c.roundRect(box_x, box_y, box_width, box_height, radius=4, stroke=1, fill=0)
+
+        # 텍스트 그리기
+        c.setFont("Pretendard-Regular", 14)  # 글꼴과 크기 설정
+        c.setFillColor(orange_color)
+        c.drawString(box_x + 6, box_y + 4, f'{problem["correctRate"]}%')  # 박스 안 텍스트 위치 조정
+
     # 점선 상자1
-    c.setStrokeColor(orange_color)
-    c.setLineWidth(0.5)
-    c.setDash(3, 3)
-    c.rect(30, 190, width - 60, 270, stroke=1, fill=0)
+    draw_dashed_box(c, 30, 190, width - 60, 270)
 
     ### 현재 등급에서 맞췄어야 하는 문제 ###
     # 세로선 그리기 (오렌지 색상)
@@ -116,6 +137,35 @@ async def create_review_note():
     text.textLine("넘어가야하는 문제들이예요.")
     c.drawText(text)
 
+    ## 현재 등급에서 맞췄어야 하는 문제
+    text_y = 355
+    for i, problem in enumerate(data["forCurrentRating"]):
+        
+        if i%5 == 0 and i > 0:
+            text_y -= 30
+        
+        c.setFillColor(black_color)
+        c.setFont("Pretendard-Regular", 18)
+        p_num = f'{problem["problemNumber"]}번'
+        c.drawString(45 + (i%5)*110, text_y, p_num)
+
+        p_num_width = c.stringWidth(p_num, "Pretendard-Regular", 18)
+
+        box_width = 40
+        box_height = 20
+        box_x = 50 + (i%5)*110 + p_num_width
+        box_y = text_y - 4
+
+        # 박스 그리기 (배경)
+        c.setLineWidth(1)  # 테두리 두께 설정 (얇게)
+        c.setStrokeColor(HexColor("#FFA500"))  # 주황색 테두리
+        c.roundRect(box_x, box_y, box_width, box_height, radius=4, stroke=1, fill=0)
+
+        # 텍스트 그리기
+        c.setFont("Pretendard-Regular", 14)  # 글꼴과 크기 설정
+        c.setFillColor(orange_color)
+        c.drawString(box_x + 6, box_y + 5, f'{problem["correctRate"]}%')  # 박스 안 텍스트 위치 조정
+
     ### 다음 등급을 위해 맞춰야 하는 문제 ###
     # 세로선 그리기 (오렌지 색상)
     c.setDash([])
@@ -132,11 +182,36 @@ async def create_review_note():
     text.textLine("다음 등급에서 맞춰야하는 문제들이예요.")
     c.drawText(text)
 
+    text_y = 240
+    for i, problem in enumerate(data["forNextRating"]):
+        
+        if i%5 == 0 and i > 0:
+            text_y -= 30
+        
+        c.setFillColor(black_color)
+        c.setFont("Pretendard-Regular", 18)
+        p_num = f'{problem["problemNumber"]}번'
+        c.drawString(45 + (i%5)*110, text_y, p_num)
+
+        p_num_width = c.stringWidth(p_num, "Pretendard-Regular", 18)
+
+        box_width = 40
+        box_height = 20
+        box_x = 50 + (i%5)*110 + p_num_width
+        box_y = text_y - 4
+
+        # 박스 그리기 (배경)
+        c.setLineWidth(1)  # 테두리 두께 설정 (얇게)
+        c.setStrokeColor(HexColor("#FFA500"))  # 주황색 테두리
+        c.roundRect(box_x, box_y, box_width, box_height, radius=4, stroke=1, fill=0)
+
+        # 텍스트 그리기
+        c.setFont("Pretendard-Regular", 14)  # 글꼴과 크기 설정
+        c.setFillColor(orange_color)
+        c.drawString(box_x + 6, box_y + 5, f'{problem["correctRate"]}%')
+
     # 점선 상자2
-    c.setStrokeColor(orange_color)
-    c.setLineWidth(0.5)
-    c.setDash(3, 3)
-    c.rect(30, 40, width - 60, 140, stroke=1, fill=0)
+    draw_dashed_box(c, 30, 40, width - 60, 140)
 
     ### 등급 다지기 ###
     # 세로선 그리기 (오렌지 색상)
@@ -154,5 +229,138 @@ async def create_review_note():
     text.textLine("현재 등급 이상을 안정적으로 받기 위해 체크해볼 만한 문제예요.")
     c.drawText(text)
 
+    text_y = 90
+    for i, problem in enumerate(data["forBeforeRating"]):
+        if i%5 == 0 and i > 0:
+            text_y -= 30
+        
+        c.setFillColor(black_color)
+        c.setFont("Pretendard-Regular", 18)
+        p_num = f'{problem["problemNumber"]}번'
+        c.drawString(45 + (i%5)*110, text_y, p_num)
+
+        p_num_width = c.stringWidth(p_num, "Pretendard-Regular", 18)
+
+        box_width = 40
+        box_height = 20
+        box_x = 50 + (i%5)*110 + p_num_width
+        box_y = text_y - 4
+
+        # 박스 그리기 (배경)
+        c.setLineWidth(1)  # 테두리 두께 설정 (얇게)
+        c.setStrokeColor(HexColor("#FFA500"))  # 주황색 테두리
+        c.roundRect(box_x, box_y, box_width, box_height, radius=4, stroke=1, fill=0)
+
+        # 텍스트 그리기
+        c.setFont("Pretendard-Regular", 14)  # 글꼴과 크기 설정
+        c.setFillColor(orange_color)
+        c.drawString(box_x + 6, box_y + 5, f'{problem["correctRate"]}%')
+
+
+    all_wrong_problems = data["forCurrentRating"] + data["forNextRating"] + data["forBeforeRating"]
+    cnt1, cnt2, cnt3 = len(data["forCurrentRating"]), len(data["forNextRating"]), len(data["forBeforeRating"])
+
+    page_num = 2
+    
+    
+    for problem_data in data["forCurrentRating"]:
+        c.showPage()
+        draw_problem_page(c, data=problem_data, page_number=page_num, flag=0)
+        page_num += 1
+
+    for problem_data in data["forNextRating"]:
+        c.showPage()
+        draw_problem_page(c, data=problem_data, page_number=page_num, flag=1)
+        page_num += 1
+
+    for problem_data in data["forBeforeRating"]:
+        c.showPage()
+        draw_problem_page(c, data=problem_data, page_number=page_num, flag=2)
+        page_num += 1
     
     c.save()
+
+# create_review_note({
+#   "testResultId": 2688,
+#   "score": 83,
+#   "solvingTime": "PT1H11M",
+#   "averageSolvingTime": "PT1H13M10.588235294S",
+#   "estimatedRatingGetResponses": [
+#     {
+#       "ratingProvider": "대성마이맥",
+#       "estimatedRating": 2
+#     },
+#     {
+#       "ratingProvider": "이투스",
+#       "estimatedRating": 2
+#     }
+#   ],
+#   "incorrectProblems": [
+#     {
+#       "problemNumber": "3",
+#       "correctRate": 85
+#     },
+#     {
+#       "problemNumber": "25",
+#       "correctRate": 81
+#     },
+#     {
+#       "problemNumber": "27",
+#       "correctRate": 69
+#     },
+#     {
+#       "problemNumber": "28",
+#       "correctRate": 44
+#     },
+#     {
+#       "problemNumber": "29",
+#       "correctRate": 13
+#     }
+#   ],
+#   "forCurrentRating": [{
+#       "problemNumber": "3",
+#       "difficultLevel": "중하",
+#       "correctRate": 85,
+#       "rating": "4등급",
+#       "imageUrl": "https://s3-moplus.s3.ap-northeast-2.amazonaws.com/2025%ED%95%99%EB%85%84%EB%8F%84%20%EA%B3%A03%2010%EC%9B%94%20%EB%AA%A8%ED%8F%89%20%ED%99%95%EB%A5%A0%EA%B3%BC%20%ED%86%B5%EA%B3%84/738.png"
+#     },
+#     {
+#       "problemNumber": "25",
+#       "difficultLevel": "중하",
+#       "correctRate": 81,
+#       "rating": "4등급",
+#       "imageUrl": "https://s3-moplus.s3.ap-northeast-2.amazonaws.com/2025%ED%95%99%EB%85%84%EB%8F%84%20%EA%B3%A03%2010%EC%9B%94%20%EB%AA%A8%ED%8F%89%20%ED%99%95%EB%A5%A0%EA%B3%BC%20%ED%86%B5%EA%B3%84/760.png"
+#     }],
+#   "forNextRating": [
+#     {
+#       "problemNumber": "28",
+#       "difficultLevel": "상",
+#       "correctRate": 44,
+#       "rating": "1등급",
+#       "imageUrl": "https://s3-moplus.s3.ap-northeast-2.amazonaws.com/2025%ED%95%99%EB%85%84%EB%8F%84%20%EA%B3%A03%2010%EC%9B%94%20%EB%AA%A8%ED%8F%89%20%ED%99%95%EB%A5%A0%EA%B3%BC%20%ED%86%B5%EA%B3%84/763.png"
+#     }
+#   ],
+#   "forBeforeRating": [
+#     {
+#       "problemNumber": "3",
+#       "difficultLevel": "중하",
+#       "correctRate": 85,
+#       "rating": "4등급",
+#       "imageUrl": "https://s3-moplus.s3.ap-northeast-2.amazonaws.com/2025%ED%95%99%EB%85%84%EB%8F%84%20%EA%B3%A03%2010%EC%9B%94%20%EB%AA%A8%ED%8F%89%20%ED%99%95%EB%A5%A0%EA%B3%BC%20%ED%86%B5%EA%B3%84/738.png"
+#     },
+#     {
+#       "problemNumber": "25",
+#       "difficultLevel": "중하",
+#       "correctRate": 81,
+#       "rating": "4등급",
+#       "imageUrl": "https://s3-moplus.s3.ap-northeast-2.amazonaws.com/2025%ED%95%99%EB%85%84%EB%8F%84%20%EA%B3%A03%2010%EC%9B%94%20%EB%AA%A8%ED%8F%89%20%ED%99%95%EB%A5%A0%EA%B3%BC%20%ED%86%B5%EA%B3%84/760.png"
+#     },
+#     {
+#       "problemNumber": "27",
+#       "difficultLevel": "중",
+#       "correctRate": 69,
+#       "rating": "3등급",
+#       "imageUrl": "https://s3-moplus.s3.ap-northeast-2.amazonaws.com/2025%ED%95%99%EB%85%84%EB%8F%84%20%EA%B3%A03%2010%EC%9B%94%20%EB%AA%A8%ED%8F%89%20%ED%99%95%EB%A5%A0%EA%B3%BC%20%ED%86%B5%EA%B3%84/762.png"
+#     }
+#   ]
+# })
