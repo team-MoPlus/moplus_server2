987a1ee11072ec016be3eb3c6122b980010880c2
seojm <myungs8562@gmail.com>
2024-12-03 20:16:51 +0900

    Fix: db 관련 내용 제거

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import io
-from typing import Union, List
-from fastapi.responses import FileResponse, StreamingResponse
-from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from time import time
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,22 +6,17 @@ from dotenv import load_dotenv
 from models import DetailResultApplication, PDFBody, TestResult
 from models import TestResult
 from pdfmain import create_review_note
-from database import SessionLocal, engine
-from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException
-import crud, models
 import httpx
 import asyncio
 import os
-from reportlab.pdfgen import canvas
-from urllib.parse import quote
 
 
 load_dotenv()
 
 API_URL = os.getenv("API_URL")
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -37,21 +29,18 @@ app.add_middleware(
 )
 
 
-# Dependency Injection 
-def get_db():
-    db = SessionLocal()
-    try : 
-        yield db
-    finally:
-        db.close()
+# # Dependency Injection 
+# def get_db():
+#     db = SessionLocal()
+#     try : 
+#         yield db
+#     finally:
+#         db.close()
 
 async def requestPracticeTests(client):
     response = await client.get(f"{API_URL}/practiceTests/2")
     return response.text
 
-# async def requestRating(client, practiceTestId):
-#     response = await client.get(f"{API_URL}/rating/{practiceTestId}")
-#     return response.text
 
 async def task():
     async with httpx.AsyncClient() as client:
@@ -89,21 +78,6 @@ async def get_rating(practiceTestId: str):
         response = await client.get(f"{API_URL}/rating/{practiceTestId}")
     return response.text
 
-@app.get("/image-url")
-def get_image_url(problem_id: int, db:Session=Depends(get_db)):
-    problem_image_info = crud.get_image(db, problem_id)
-    return problem_image_info.image_url
-
-# 복습표 생성 요청
-# @app.post("/request-review")
-# async def request_review_note():
-#     try:
-#         # problem_info = crud.get_image(db, skip=0, limit=0)
-#         await create_review_note()
-
-#         return {"status": "OK"}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
     
 
 # PDF 다운로드 엔드포인트
@@ -111,9 +85,3 @@ def get_image_url(problem_id: int, db:Session=Depends(get_db)):
 async def download_review_note(data: PDFBody):
     buffer = io.BytesIO()
     return create_review_note(data.test_result, data.file_name, buffer)
-
-    # headers = {
-    #     "Content-Disposition": f"attachment; filename*=UTF-8''{data.file_name}",
-    # }
-
-    # return StreamingResponse(buffer, headers=headers, media_type="application/pdf")
